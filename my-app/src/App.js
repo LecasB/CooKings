4b0e4ba9854ed3f932ba725020cc11e9143c7087
luish <luismariohernandezsantos@gmail.com>
2024-04-04 15:59:02 +0100

    card procurar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import logo from "./logo.svg";
 import CardInicial from "./componentes/CardInicial";
 import "./App.css";
 import LoginButton from "./componentes/LoginButton";
+import CardProcurar from "./componentes/CardProcurar";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
+	return (
+		<div className="App">
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,26 +21,35 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <div className="box">
-        <h2>Recommended for you</h2>
-        <div className="boxcard">
-          <CardInicial titulo={"Titilo 1"} texto={"texto 1"} />
-          <CardInicial titulo={"Titilo 2"} texto={"texto 2"} />
-          <CardInicial titulo={"Titilo 3"} texto={"texto 3"} />
-        </div>
-        <LoginButton />
-      </div>
+			<div className="box">
+				<h2>Recommended for you</h2>
+				<div className="boxcard">
+					<CardInicial titulo={"Titilo 1"} texto={"texto 1"} />
+					<CardInicial titulo={"Titilo 2"} texto={"texto 2"} />
+					<CardInicial titulo={"Titilo 3"} texto={"texto 3"} />
+				</div>
+				<LoginButton />
+			</div>
 
-      <div className="box">
-        <h2>Recently added</h2>
-        <div className="boxcard">
-          <CardInicial titulo={"Titilo 4"} texto={"texto 4"} />
-          <CardInicial titulo={"Titilo 5"} texto={"texto 5"} />
-          <CardInicial titulo={"Titilo 6"} texto={"texto 6"} />
-        </div>
-      </div>
-    </div>
-  );
+			<div className="box">
+				<h2>Recently added</h2>
+				<div className="boxcard">
+					<CardInicial titulo={"Titilo 4"} texto={"texto 4"} />
+					<CardInicial titulo={"Titilo 5"} texto={"texto 5"} />
+					<CardInicial titulo={"Titilo 6"} texto={"texto 6"} />
+				</div>
+			</div>
+
+			<div className="box">
+				<h2>Procurar</h2>
+				<div className="boxcard">
+					<CardProcurar titulo={"Titilo 4"} texto={"texto 4"} />
+					<CardProcurar titulo={"Titilo 5"} texto={"texto 5"} />
+					<CardProcurar titulo={"Titilo 6"} texto={"texto 6"} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;

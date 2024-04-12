9159cd34eaee3cc8048ad6dce32073d59e99826b
Tiago <tiago@exemplo.pt>
2024-04-12 09:44:44 +0100

    user op

--- my-app/src/componentes/UserPage/UserInfo.jsx
+++ my-app/src/componentes/UserPage/UserInfo.jsx
@@ -3,29 +3,42 @@ import "../../estilos/UserLateral.css";
 import { Profile } from "../../imagens/svgs";
 
 const UserInfo = () => {
-	return (
-		<div className="UserInfo">
-			<section className="">
-				<figure className="">
-					<Profile />
-				</figure>
+  const OpUser = ["Food"];
 
-				<label htmlFor="">
-					<input type="text" name="Nome" id="" />
-				</label>
-				<label htmlFor="">
-					<input type="email" name="Email" id="" />
-				</label>
-				<button type="button">Change Password</button>
-				<select name="Categories" id="">
-					<option value="" selected disabled hidden>Choose here</option>
-					<option value="Chinese">Chinese Food</option>
-					<option value="Spicy">Spicy</option>
-					<option value="Vegan">Vegan</option>
-				</select>
-			</section>
-		</div>
-	);
+  return (
+    <div className="UserInfo">
+      <section className="">
+        <figure className="">
+          <Profile />
+        </figure>
+
+        <label htmlFor="">
+          <input type="text" name="Nome" id="" />
+        </label>
+        <label htmlFor="">
+          <input type="email" name="Email" id="" />
+        </label>
+        <button type="button">Change Password</button>
+        <select name="Categories" id="">
+          <option value="" selected disabled hidden>
+            Choose here
+          </option>
+          <option value="Chinese">Chinese Food</option>
+          <option value="Spicy">Spicy</option>
+          <option value="Vegan">Vegan</option>
+        </select>
+        <div className="user-options-section">
+          {OpUser.map((op, index) => {
+            return (
+              <div className="user-options">
+                <p>{op}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default UserInfo;

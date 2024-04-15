7224f7b0f77f8f5af522b674ee1e4900cb47178f
luish <luismariohernandezsantos@gmail.com>
2024-04-15 15:32:14 +0100

    BoasText fixed

--- my-app/src/componentes/IndexPage/BoasText.jsx
+++ my-app/src/componentes/IndexPage/BoasText.jsx
@@ -5,11 +5,9 @@ const BoasText = () => {
 	if (horas >= 0 && horas < 12) {
 		return <p className="boas">Good Morning, Chef!</p>;
 	} else if (horas >= 12 && horas < 18) {
-		return <p className="boas">Good Morning, Chef!</p>;
-		return <p >Good Afternoon, Chef!</p>;
+		return <p className="boas">Good Afternoon, Chef!</p>;
 	} else if (horas >= 18 && horas < 24) {
-		return <p className="boas">Good Morning, Chef!</p>;
-		return <p >Good Night, Chef!</p>;
+		return <p className="boas">GGood Night, Chef!</p>;
 	}
 };
 

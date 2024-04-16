9eb004b772c2eae89b5eaca9d9092886a02de038
Leonardo Carvalho <cleonardo191@gmail.com>
2024-04-16 22:39:19 +0100

    Primeiros Testes | Supabase & MaterialUI

--- my-app/src/componentes/LoginPage/LoginForm.jsx
+++ my-app/src/componentes/LoginPage/LoginForm.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import LoginButton from './LoginButton';
-import InputText from './InputText';
-import "../../estilos/LoginForm.css"
-import Logo from '../../imagens/cooKingsImagev1.png';
+import React from "react";
+import LoginButton from "./LoginButton";
+import InputText from "./InputText";
+import "../../estilos/LoginForm.css";
+import Logo from "../../imagens/cooKingsImagev1.png";
+import Button from "@mui/material/Button";
+import Input from '@mui/material/Input';
 
 const LoginForm = () => {
-    return(
-        <div id="LoginForm">
-        <img src={Logo}></img>
-        <InputText texto={"Username"} />
-        <InputText texto={"Password"}  />
-        <InputText texto={"Confirm Password"} />
-        <LoginButton texto={"Create Account"}/>
-        <LoginButton texto={"Log in"}/> 
-        </div>
-           )
-}
-
+  return (
+    <div id="LoginForm">
+      
+      <img id="logo" src={Logo}></img>
+      <Input id="username" placeholder="Username" variant="contained"></Input>
+      <Input placeholder="Password" variant="contained"></Input>
+      <Input placeholder="Confirm Password" variant="contained"></Input>
+      <Button id="login-btn" variant="contained">Log In</Button>
+      <Button id="create-btn" variant="contained">Create Account</Button>
+    </div>
+  );
+};
 
 export default LoginForm;
---
+++ my-app/src/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+export const supabase = createClient(
+
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_API_KEY
+
+)

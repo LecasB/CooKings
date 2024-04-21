ff8df85d16382be998a7501de9b1235357c5f3f0
Leonardo Carvalho <cleonardo191@gmail.com>
2024-04-21 12:57:09 +0100

    Fixing Loading, Login and SignUp styling

--- my-app/src/componentes/LoginPage/LoginForm.jsx
+++ my-app/src/componentes/LoginPage/LoginForm.jsx
@@ -4,15 +4,27 @@ import "../../estilos/SignUpForm.css";
 import Logo from "../../imagens/cooKingsImagev1.png";
 import LoginButton from "../LoginPage/LoginButton";
 import InputText from "./InputText";
+import CheckBox from "./CheckBox";
+import Text from "./Text";
+
 
 const LoginForm = () => {
   return (
+    <div id="page">
     <div id="SignUpForm">
       <img src={Logo}></img>
       <InputText texto={"Username"} />
       <InputText texto={"Password"} />
-      <LoginButton texto={"Create Account"} />
+      <CheckBox></CheckBox>
+      <Text texto="Forgot your Password?"></Text>
       <LoginButton texto={"Log in"} />
+      <Text texto="Or"></Text>
+      <Text texto="Dont have a account yet?"></Text>
+      <LoginButton texto={"Create Account"} />
+      </div>
+      
+    
+      
     </div>
   );
 };
--- my-app/src/componentes/SignUpPage/SignUpForm.jsx
+++ my-app/src/componentes/SignUpPage/SignUpForm.jsx
@@ -4,25 +4,24 @@ import LoginButton from "../LoginPage/LoginButton";
 import InputText from "./InputText";
 import "../../estilos/SignUpForm.css";
 import "../../estilos/LoginForm.css";
+import Text from "./Text";
 
 const SignUpForm = () => {
   return (
-    <div id="LoginForm">
-      <img id="logo" src={Logo}></img>
-      <InputText
-        id="username"
-        placeholder="Username"
-        variant="contained"
-      ></InputText>
-      <InputText placeholder="Password" variant="contained"></InputText>
-      <InputText placeholder="Confirm Password" variant="contained"></InputText>
-      <LoginButton texto={"Log In"} id="login-btn" variant="contained" />
-      <LoginButton
-        texto={"Create Account"}
-        id="create-btn"
-        variant="contained"
-      />
+    <div id="page">
+    <div id="SignUpForm">
+    <img src={Logo}></img>
+    <InputText texto={"Username"} />
+    <InputText texto={"Password"} />
+    <InputText texto={"Confirm Password"} />
+    <LoginButton texto={"Create Account"} />
+    <Text texto="Or"></Text>
+    <LoginButton texto={"Log in"} />
     </div>
+  
+    
+  </div>
+  
   );
 };
 

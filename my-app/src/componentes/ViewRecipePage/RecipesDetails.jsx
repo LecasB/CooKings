c91710d7b4e59a62f43c3042ff949f1b71a7f667
Leonardo Carvalho <cleonardo191@gmail.com>
2024-05-26 01:01:48 +0100

    Fix route

--- my-app/src/componentes/ViewRecipePage/RecipesDetails.jsx
+++ my-app/src/componentes/ViewRecipePage/RecipesDetails.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "../../estilos/RecipeDetails.css";
+import "./RecipesDetails.css";
 
 const RecipeDetails = () => {
 	return (
-		<>
 			<main>
 				<div className="ViewRecipePage">
 					<div class="Recipe">
@@ -20,7 +19,6 @@ const RecipeDetails = () => {
 					</div>
 				</div>
 			</main>
-		</>
 	);
 };
 

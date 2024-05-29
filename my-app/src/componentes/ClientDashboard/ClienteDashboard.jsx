559b1d542f10439c561dfc8697633049cbbbaf63
Tiago <tiago@exemplo.pt>
2024-05-29 11:07:25 +0100

    Editei alguns estilos

--- my-app/src/componentes/ClientDashboard/ClienteDashboard.jsx
+++ my-app/src/componentes/ClientDashboard/ClienteDashboard.jsx
@@ -95,36 +95,17 @@ const ClientDashboard = () => {
       <h3>You got {fav} favourites</h3>
       <h3>You have {produtos.length} Ingredients</h3> */}
       <div className="cards-display">
-        <div className="card-clientdashboard-expire">
+        <div className="card-clientdashboard">
           <div className="card-clientdashboard-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-              <path
-                fill="#e80000"
-                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z" />
             </svg>
           </div>
           <div className="card-clientdashboard-text">
-            <p className="card-clientdashboard-p">Ingredients Expired: </p>
-            <p className="card-clientdashboard-total">{expire}</p>
+            <p className="card-clientdashboard-total">{produtos.length}</p>
+            <p className="card-clientdashboard-p">Total Ingredients</p>
           </div>
         </div>
-
-        <div className="card-clientdashboard-expiring">
-          <div className="card-clientdashboard-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path
-                fill="#ffaf36"
-                d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
-              />
-            </svg>
-          </div>
-          <div className="card-clientdashboard-text">
-            <p className="card-clientdashboard-p">Ingredients Expiring: </p>
-            <p className="card-clientdashboard-total">{expiring}</p>
-          </div>
-        </div>
-
         <div className="card-clientdashboard-valid">
           <div className="card-clientdashboard-icon">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -135,8 +116,36 @@ const ClientDashboard = () => {
             </svg>
           </div>
           <div className="card-clientdashboard-text">
-            <p className="card-clientdashboard-p">Ingredients Valid: </p>
             <p className="card-clientdashboard-total">{valid}</p>
+            <p className="card-clientdashboard-p">Ingredients Valid</p>
+          </div>
+        </div>
+        <div className="card-clientdashboard-expiring">
+          <div className="card-clientdashboard-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path
+                fill="#ffaf36"
+                d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
+              />
+            </svg>
+          </div>
+          <div className="card-clientdashboard-text">
+            <p className="card-clientdashboard-total">{expiring}</p>
+            <p className="card-clientdashboard-p">Ingredients Expiring</p>
+          </div>
+        </div>
+        <div className="card-clientdashboard-expire">
+          <div className="card-clientdashboard-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+              <path
+                fill="#e80000"
+                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+              />
+            </svg>
+          </div>
+          <div className="card-clientdashboard-text">
+            <p className="card-clientdashboard-total">{expire}</p>
+            <p className="card-clientdashboard-p">Ingredients Expired</p>
           </div>
         </div>
 
@@ -150,23 +159,8 @@ const ClientDashboard = () => {
             </svg>
           </div>
           <div className="card-clientdashboard-text">
-            <p className="card-clientdashboard-p">Favorites: </p>{" "}
             <p className="card-clientdashboard-total">{fav}</p>
-          </div>
-        </div>
-
-        <div className="card-clientdashboard">
-          <div className="card-clientdashboard-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-              <path
-                fill="#e80000"
-                d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-              />
-            </svg>
-          </div>
-          <div className="card-clientdashboard-text">
-            <p className="card-clientdashboard-p">Total Ingredients: </p>
-            <p className="card-clientdashboard-total">{produtos.length}</p>
+            <p className="card-clientdashboard-p">Favorites</p>
           </div>
         </div>
       </div>

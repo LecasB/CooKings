1bd11bb07b431959d2ad98fd08ff74a80c50de1b
Leonardo Carvalho <cleonardo191@gmail.com>
2024-05-31 13:56:32 +0100

    Fixed SVG

--- my-app/src/componentes/IndexPage/IndexPage.jsx
+++ my-app/src/componentes/IndexPage/IndexPage.jsx
@@ -86,7 +86,7 @@ const IndexPage = () => {
             </figure>
           </div>
         </div>
-        <div className="boxcard">
+        <div className="boxcard fundoazul">
           <UserPossibility
             logo={
               <svg

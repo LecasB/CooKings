ec79060f2612adbae46ec492bef309492eecb11b
Tiago <tiago@exemplo.pt>
2024-05-31 13:52:02 +0100

    Verificar se é admin para entrar no AdminDashboard

--- my-app/src/componentes/NavBar.jsx
+++ my-app/src/componentes/NavBar.jsx
@@ -3,11 +3,14 @@ import cooKingsImage from "../imagens/cooKingsImagev1.png";
 import "../estilos/NavBar.css";
 import { Storage, Profile } from "../imagens/svgs.jsx";
 import OverlayMenu from "./OverlayMenu.jsx";
+import supabase from "../supabaseClient";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  let mediaQuery = window.matchMedia("(max-width: 1980px)"); //por enquanto deixo sempre ativo para podermos andar pela aplicação
+  let mediaQuery = window.matchMedia("(max-width: 980px)"); //por enquanto deixo sempre ativo para podermos andar pela aplicação
   const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState();
+  const [userAdmin, setAdmin] = useState(false);
 
   const ativarMenu = () => {
     setIsOpen(!isOpen);
@@ -32,6 +35,23 @@ const NavBar = () => {
       }
     }
   };
+
+  const getUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+      setUser(user);
+      setAdmin(user.user_metadata.admin);
+    } else {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   useEffect(() => {
     hamburguer();
@@ -65,12 +85,35 @@ const NavBar = () => {
         <div>
           <ul className="linksContainer">
             <li className="linksItems">
-              <Link to="IngredientsListPage/IngredientsListPage">
+              <Link to={"/SearchPage"}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                </svg>
+              </Link>
+            </li>
+            <li className="linksItems">
+              <Link
+                to={
+                  user == null
+                    ? "/SignUpPage"
+                    : userAdmin
+                    ? "/AdminDashboardPage/ListaReceitas"
+                    : "/ClientDashboard/ListaIngredienteClient"
+                }
+              >
                 <Storage />
               </Link>
             </li>
             <li className="linksItems">
-              <Link to="/UserPage">
+              <Link
+                to={
+                  user == null
+                    ? "/SignUpPage"
+                    : userAdmin
+                    ? "/AdminDashboardPage"
+                    : "/ClientDashboard"
+                }
+              >
                 <Profile />
               </Link>
             </li>
